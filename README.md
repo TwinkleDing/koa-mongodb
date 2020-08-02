f87c7e322f82ce2357d2e897accc96dbc5e76076
TinkleDing <twinkle415@163.com>
2020-08-02 20:06:09 +0800

    readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # koa-mongodb
 node+koa+mongodb的后端
+
+## process
+process核心模块提供了一种便利的的方法，可以以编程的方式退出node.js：pprocess.exit()。
+w
+w
+ - 12
+ 2  
+2
+ 2
--- bin/www.js
+++ bin/www.js
@@ -3,7 +3,7 @@ const debug = require('debug')('demo:server');
 const http = require('http');
 
 // 设置端口号3333
-const port = normalizePort( process.env.PORT || "3333");
+const port = normalizePort(process.env.PORT || "3333");
 const server = http.createServer(app.callback());
 
 // 监听端口号
@@ -12,7 +12,7 @@ server.on('error', onError);
 server.on('listening', onListening);
 
 function normalizePort(val){
-  let port = parseInt(val,10);
+  let port = parseInt(val, 10);
   if(isNaN(port)) return port;
   if(port >= 0) return port;
   return false;
@@ -21,7 +21,7 @@ function normalizePort(val){
 function onError(error){
   if( error.syscall !== 'listen') throw error;
 
-  let bind = typeof port == 'string'?'Pipe ' + port: 'port ' + port;
+  let bind = typeof port == 'string' ? 'Pipe ' + port : 'port ' + port;
   
   switch(error.code){
     case 'EACCES':
