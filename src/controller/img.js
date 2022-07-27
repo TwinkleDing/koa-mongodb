5bca2c0604f3995bc8a2ad3d31b01e3035b18503
dingyuliang <dingyuliang>
2022-07-27 13:49:43 +0800

    去掉上传就添加到个人信息

--- src/controller/img.js
+++ src/controller/img.js
@@ -1,23 +1,16 @@
 const xss = require("xss");
 const Img = require('../db').Img;
-const User = require('../db').User;
 
 module.exports = {
     // 上传头像
     async upload(ctx, next) {
-        let token = ctx.get("Authorization");
         let file = ctx.request.files.file
-        let user = await User.findOne({
-            token
-        });
         try {
             if (file.filepath) {
-                // 将图片添加到 个人信息中
-                user.avatar = file.filepath
-                user.save();
                 ctx.body = {
                     code: 200,
-                    msg: '上传成功！'
+                    msg: '上传成功！',
+                    data: file.filepath
                 }
             } else {
                 ctx.body = {
